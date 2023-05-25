c123a599a7e54a91f704256f461146a84091a41c
EC2 Default User <ec2-user@ip-172-31-6-135.ap-northeast-1.compute.internal>
2023-05-25 08:13:36 +0000

    kintaib heroku 修正

--- app/models/attendance.rb
+++ app/models/attendance.rb
@@ -8,6 +8,10 @@ class Attendance < ApplicationRecord
   validate :finished_at_is_invalid_without_a_started_at
   # 出勤・退勤時間どちらも存在する時、出勤時間より早い退勤時間は無効
   validate :started_at_than_finished_at_fast_if_invalid
+  # 退勤時間が存在しない場合、出勤時間は無効（勤怠編集画面からの更新の場合のみ適用）
+  validate :started_at_is_invalid_without_a_finished_at, if: :editing
+
+  attr_accessor :editing
 
   def finished_at_is_invalid_without_a_started_at
     errors.add(:started_at, "が必要です") if started_at.blank? && finished_at.present?
@@ -16,6 +20,12 @@ class Attendance < ApplicationRecord
   def started_at_than_finished_at_fast_if_invalid
     if started_at.present? && finished_at.present?
       errors.add(:started_at, "より早い退勤時間は無効です") if started_at > finished_at
+    end
+  end
+  
+  def started_at_is_invalid_without_a_finished_at
+    if editing || worked_on != Date.today
+      errors.add(:finished_at, "が必要です") if started_at.present? && finished_at.blank?
     end
   end
 end
